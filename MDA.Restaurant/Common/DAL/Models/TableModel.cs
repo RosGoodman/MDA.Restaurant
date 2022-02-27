d171e5d36266ba117b8d57100b3b4f5d84b66808
Pavel <pavel.official@bk.ru>
2022-02-27 15:42:46 +0300

    added proj

---
+++ MDA.Restaurant/Common/DAL/Models/TableModel.cs
@@ -0,0 +1,30 @@
+﻿
+using Common.Enums;
+
+namespace Common.DAL.Models;
+
+/// <summary> Модель опимсывающая стол ресторана. </summary>
+public class TableModel
+{
+    public int Id { get; set; }
+    public State State { get; private set; }
+    public int SeatsCount { get; }
+
+    public TableModel(int id)
+    {
+        Id = id;
+        State = State.Free;
+        SeatsCount = new Random().Next(2, 5);
+    }
+
+    /// <summary> Назначить состояние стола. </summary>
+    /// <param name="state"> Состояние стола. </param>
+    /// <returns> Состояние стола измениелось/не изменилость (true/false). </returns>
+    public bool SetState(State state)
+    {
+        if (state == State) return false;
+
+        State = state;
+        return true;
+    }
+}
---
+++ MDA.Restaurant/Common/Enums/TableState.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Common.Enums;
+
+/// <summary> Перечисление состояний стола. </summary>
+public enum State
+{
+    /// <summary> Стол свобден. </summary>
+    Free,
+    /// <summary> Стол занят. </summary>
+    Booked
+}
